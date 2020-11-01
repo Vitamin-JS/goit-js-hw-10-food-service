bc134968b903ad7c8f612b13492c5c431b4cd79d
Vitamin-JS <Vpopchenkov@gmail.com>
2020-11-01 02:10:28 +0200

    Add Page Markup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import './styles.css';
-import template from './template.hbs';
-import menu from './menu.json';
+import menuTemplate from './template.hbs';
+import menuElement from './menu.json';
+
+const refs = {
+    menu: document.querySelector('.js-menu')
+};
 
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
 };
+
+
+// ------ Page Narkup------------------------------
+const menuMarkup = createMenu(menuElement);
+
+function createMenu(menuElement) {
+    return menuElement.map(menuTemplate).join('');
+};
+
+refs.menu.insertAdjacentHTML('beforeend', menuMarkup);
